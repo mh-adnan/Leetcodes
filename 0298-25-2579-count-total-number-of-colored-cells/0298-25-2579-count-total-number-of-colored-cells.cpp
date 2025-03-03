7c9de01a72e96e372b1360ae48fd82571405e134
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2025-03-03 16:43:35 +0600

    Time: 0 ms, Memory: 7.9 MB - LeetHub

--- 0298-25-2579-count-total-number-of-colored-cells/0298-25-2579-count-total-number-of-colored-cells.cpp
+++ 0298-25-2579-count-total-number-of-colored-cells/0298-25-2579-count-total-number-of-colored-cells.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     long long coloredCells(long long int n) {
-        return n * n + (n - 1) * (n - 1);
+        long long int k = n - 1;
+        return n * n + (k * k);
     }
 };
