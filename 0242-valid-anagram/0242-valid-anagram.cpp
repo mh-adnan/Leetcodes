65232953add9f0602e024cbe6c64f525d068140d
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2025-08-30 11:11:52 +0600

    Time: 0 ms (100%), Space: 10 MB (11.61%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,27 +1,22 @@
 class Solution {
 public:
-    bool isAnagram(string s, string t) 
-    {
-    vector<char>v1,v2;
-    for(int i=0;i<s.size();i++)
-    {
-        char a = s[i];
-        v1.push_back(a);
-    }
+    bool isAnagram(string s, string t) {
+        unordered_map<int, int> x, y;
+        for (int a : s) {
+            x[a]++;
+        }
 
-     for(int i=0;i<t.size();i++)
-    {
-        char a = t[i];
-        v2.push_back(a);
-    }
-    sort(v1.begin(),v1.end());
-    sort(v2.begin(),v2.end());
+        for (int b : t) {
+            y[b]++;
+        }
 
-    if(v1==v2)
-    {
+        for(int i=0;i<x.size();i++)
+        {
+            if(x!=y)
+            {
+                return false;
+            }
+        }
         return true;
-    }
-    else return false;
-
     }
 };
