7523c0760ebd63f966c756e03303ea2a6a222afc
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2025-09-01 11:23:52 +0600

    Time: 7 ms (10.76%), Space: 14.9 MB (8.9%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) 
-    {
-     set<int>s1,s2;
-     for(int i=0;i<nums1.size();i++)
-     {
-        s1.insert(nums1[i]);
-     }  
-     for(int i=0;i<nums2.size();i++)
-     {
-        s2.insert(nums2[i]);
-     }   
-     int sz = s1.size()+ s2.size();
-     vector<int>res;
-     set_intersection(s1.begin(),s1.end(),s2.begin(),s2.end(),back_inserter(res));
-     return res;
-    }
+    vector<int> intersection(vector<int>& nums1, vector<int>& nums2) {
+        unordered_map<int, int> mp1, mp2;
+        for (auto x : nums1) {
+            mp1[x]++;
+        }
 
+        for (auto y : nums2) {
+            mp2[y]++;
+        }
+        vector<int> ans;
+        for (auto it : mp1) {
+            int nn = it.first;
+            if (mp2.find(nn) != mp2.end()) {
+                ans.push_back(nn);
+            }
+        }
+        return ans;
+    }
 };
