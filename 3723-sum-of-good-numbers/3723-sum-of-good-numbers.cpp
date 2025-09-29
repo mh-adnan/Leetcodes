87b6e48395b0c6af6debfb4d701c3548b4adfd73
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2025-09-29 15:22:24 +0600

    Time: 0 ms (100%), Space: 27.1 MB (89.81%) - LeetHub

---
+++ 3723-sum-of-good-numbers/3723-sum-of-good-numbers.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    int sumOfGoodNumbers(vector<int>& n, int k) {
+        vector<int> ans;
+        
+        int sum = 0;
+
+        for(int i=0;i<n.size();i++)
+        {
+            bool p = true;
+            if(i-k>=0  && n[i]<=n[i-k])
+            {
+                p = false;
+            }
+
+            if(i+k<n.size()  && n[i]<=n[i+k])
+            {
+               p = false;
+            }
+
+            if(p==true)
+            {
+                sum+=n[i];
+            }
+        }
+        return sum;
+    }
+};
+
+
+
+
+
+
+
+
+
