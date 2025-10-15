1032ddce3fbef26fb929c04357c6c1721a4ce8c9
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2025-10-15 14:45:16 +0600

    Time: 0 ms (100%), Space: 12.1 MB (13.41%) - LeetHub

--- 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
+++ 1802-number-of-students-unable-to-eat-lunch/1802-number-of-students-unable-to-eat-lunch.cpp
@@ -1,8 +1,6 @@
 class Solution {
 public:
     int countStudents(vector<int>& st, vector<int>& snd) {
-        int totStu1 = 0;
-        int p = 0;
 
         deque<int> dqst;
         deque<int> dqsnd;
@@ -10,36 +8,25 @@ public:
         dqst.insert(dqst.end(), st.begin(), st.end());
         dqsnd.insert(dqsnd.end(), snd.begin(), snd.end());
 
-        for (int i = 0; i < st.size(); i++) 
-        {
-            if (st[i] == 1) {
-                totStu1 += 1;
-            }
-        }
+        int cnt = 0;
 
-        int cnt=0;
-
-       while (!dqst.empty() && !dqsnd.empty()) 
-        {
+        while (!dqst.empty() && !dqsnd.empty()) {
             int a = dqst.front();
             int b = dqsnd.front();
 
-            if(a==b)
-            {
+            if (a == b) {
                 dqst.pop_front();
                 dqsnd.pop_front();
-                cnt=0;
+                cnt = 0;
             }
 
-            else if(a!=b)
-            {
-                
+            else if (a != b) {
+
                 dqst.pop_front();
                 dqst.push_back(a);
                 cnt++;
             }
-            if(cnt== dqst.size())
-            {
+            if (cnt == dqst.size()) {
                 break;
             }
         }
