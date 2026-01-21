563b4b627cd77d29d4bbdbff4164a3cefec66b3c
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2026-01-21 17:40:47 +0600

    Time: 3 ms (46.67%), Space: 7.8 MB (64.69%) - LeetHub

--- 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
+++ 1688-count-of-matches-in-tournament/1688-count-of-matches-in-tournament.cpp
@@ -1,21 +1,7 @@
 class Solution {
 public:
-    int numberOfMatches(int n) {
-        int ans = 0;
-
-        while (n != 1) {
-
-            if (n % 2 == 0) {
-                n = n / 2;
-                ans += n;
-            }
-
-            else if (n % 2 != 0) {
-                n = n / 2;
-                ans += n;
-                n = n + 1;
-            }
-        }
-        return ans;
+    int numberOfMatches(int n) 
+    {
+      return n-1;    
     }
 };
