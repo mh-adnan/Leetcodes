e6bf8c58f9063055d002d996917a3f6e099c9d93
Mehedi Hassan Adnan <135158805+mh-adnan@users.noreply.github.com>
2026-01-22 20:11:57 +0600

    Time: 0 ms (100%), Space: 8.6 MB (8.19%) - LeetHub

--- 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
+++ 3658-gcd-of-odd-and-even-sums/3658-gcd-of-odd-and-even-sums.cpp
@@ -1,17 +1,11 @@
 class Solution {
 public:
-    int gcdOfOddEvenSums(int n) {
-        int sum = 0;
-        int k = 0;
-        for (int i = 1; i <= k; i += 2) {
-            sum += i;
-            k++;
-            if (k == n) {
-                break;
-            }
-        }
-        int o = sum + n;
-        int m = __gcd(sum, o);
-        return m;
+    int gcdOfOddEvenSums(int n) 
+    {
+      int a = n*n;
+      int b = n*(1+n);
+
+      return __gcd(a,b);
+
     }
 };
